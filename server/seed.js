f39c741fbd49fd51715e41ff45c646826411adf7
Naomi Liu <nhliu@ucdavis.edu>
2021-03-11 23:14:53 -0800

    retrieved addresses

---
+++ server/seed.js
@@ -0,0 +1,12 @@
+// read in Json, loop over them and add them to the data base using 
+// written add function
+// when to run this?
+
+const data = require('./addresses.json'); //list of dictionaries
+const addressController = require('./controllers/address/');
+
+console.log(data)
+for (address of data){
+    addressController.add(address)
+}
+
--- server/server.js
+++ server/server.js
@@ -2,16 +2,17 @@ const { Router, request } = require('express');
 const express = require( 'express' );
 
 const app = express();
+const addressController = require('./controllers/address/');
 
 app.get( '/', ( req, res ) => {
   res.send({ greeting: 'Hello world!' });
 });
 
-app.get( '/addresses', ( req, res ) => {
+app.get( '/addresses', async ( req, res ) => {
   // get all addresses from the DB
-  res.send({ greeting: 'Hello world!' });
+  const allAddresses = await addressController.search()
+  res.send({ allAddresses });
 });
-
 
 app.delete( '/deleteaddress', ( req, res ) => {
   // delete a address from the DB
